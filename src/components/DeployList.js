1655389e56457acc6b7ed7c6ac66ea4b032438c1
Daniel Olson <emaildano@gmail.com>
2020-06-13 19:30:14 -0400

    added deploy list

---
+++ src/components/DeployList.js
@@ -0,0 +1,29 @@
+// src/components/DeployList.js
+
+import React from "react";
+
+import Deploy from "./Deploy";
+
+function DeployList({ loading, deploys, onViewDeploy }) {
+  const events = {
+    onViewDeploy,
+  };
+
+  if (loading) {
+    return <div className='list-items'>loading</div>;
+  }
+
+  if (deploys.length === 0) {
+    return <div className='list-items'>empty</div>;
+  }
+
+  return (
+    <div className='list-items'>
+      {deploys.map((deploy) => (
+        <Deploy key={deploy.id} deploy={deploy} {...events} />
+      ))}
+    </div>
+  );
+}
+
+export default DeployList;
---
+++ src/components/DeployList.stories.js
@@ -0,0 +1,40 @@
+// src/components/DeployList.stories.js
+
+import React from "react";
+
+import DeployList from "./DeployList";
+import { deployData, actionsData } from "./Deploy.stories";
+
+export default {
+  component: DeployList,
+  title: "DeployList",
+  decorators: [(story) => <div style={{ padding: "3rem" }}>{story()}</div>],
+  excludeStories: /.*Data$/,
+};
+
+export const defaultTasksData = [
+  { ...deployData, id: "1", state: "DEPLOY_BUILDING" },
+  { ...deployData, id: "2", state: "DEPLOY_PUBLISHED" },
+  { ...deployData, id: "3", state: "DEPLOY_FAILED", title: "Don't Panic."  },
+  {
+    ...deployData,
+    id: "4",
+    title: "I'd far rather be happy than right any day.",
+  },
+  {
+    ...deployData,
+    id: "5",
+    title: "Time is an illusion. Lunchtime doubly so.",
+  },
+  { ...deployData, id: "6", title: "Is there any tea on this spaceship?" },
+];
+
+export const Default = () => (
+  <DeployList deploys={defaultTasksData} {...actionsData} />
+);
+
+export const Loading = () => (
+  <DeployList loading deploys={[]} {...actionsData} />
+);
+
+export const Empty = () => <DeployList deploys={[]} {...actionsData} />;
