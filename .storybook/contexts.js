4df5a1970420135dba2486646d6e1ab47fd5008b
Daniel Olson <emaildano@gmail.com>
2020-06-25 17:53:54 -0400

    added context add-on

--- .storybook/contexts.js
+++ .storybook/contexts.js
@@ -1,38 +1,28 @@
-const createContext = (initialValue) => {
-  return {
-    name: `Context.i18n`,
-    props: ["value"],
-    watch: {
-      value: function (newValue, oldValue) {
-        this.$root._i18n.locale = newValue.locale
-      },
-    },
-    template: `<div><slot /></div>`,
-  }
-}
-
-const i18nContext = createContext({
-  locale: "en",
-})
-
 export const contexts = [
   {
-    icon: "globe",
-    title: "Languages",
-    components: [i18nContext],
+    icon: "box", // a icon displayed in the Storybook toolbar to control contextual props
+    title: "Themes", // an unique name of a contextual environment
+    components: [
+      // an array of components that is going to be injected to wrap stories
+      /* Styled-components ThemeProvider, */
+      /* Material-ui ThemeProvider, */
+    ],
     params: [
+      // an array of params contains a set of predefined `props` for `components`
       {
-        name: "English",
-        props: {
-          value: { locale: "en" },
-        },
+        name: "Light Theme",
+        props: { theme: "light" },
       },
       {
-        name: "Japanese",
-        props: {
-          value: { locale: "ja" },
-        },
+        name: "Dark Theme",
+        props: { theme: "dark" },
+        default: true,
       },
     ],
+    options: {
+      deep: true, // pass the `props` deeply into all wrapping components
+      disable: false, // disable this contextual environment completely
+      cancelable: false, // allow this contextual environment to be opt-out optionally in toolbar
+    },
   },
 ]
