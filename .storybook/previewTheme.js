8be8aaa5461c71a62b11a0432bac8547dc06d36e
Daniel Olson <emaildano@gmail.com>
2020-08-03 16:13:03 -0400

    added typekit css

--- .storybook/previewTheme.js
+++ .storybook/previewTheme.js
@@ -1,20 +1,21 @@
 // .storybook/preview.js
-import React from "react"
-import { addDecorator } from "@storybook/react"
-import { withContexts } from "@storybook/addon-contexts/react"
-import styled, { createGlobalStyle, ThemeProvider } from "styled-components"
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { withContexts } from '@storybook/addon-contexts/react';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
 import theme from 'styled-theming';
 
-import { GlobalStyle } from "./../src/styles/index"
-import { contexts } from "./contexts"
-import { themes } from "../src/styles"
+import { GlobalStyle } from './../src/styles/index';
+import { contexts } from './contexts';
+import { themes } from '../src/styles';
 
-addDecorator(withContexts(contexts))
-addDecorator((storyFn) => (
-  <div>
+addDecorator(withContexts(contexts));
+addDecorator(storyFn => (
+  <>
+    <link rel="stylesheet" href="https://use.typekit.net/rrt0shs.css" />
     <ThemeProvider theme={{ mode: 'light' }}>
       <GlobalStyle />
       {storyFn()}
     </ThemeProvider>
-  </div>
-))
+  </>
+));
