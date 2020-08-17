2171539bea99d017066eeccf0e743f2cbbea49aa
Hidetaka Okamoto <info@wp-kyoto.net>
2020-08-17 10:41:35 +0900

    chore: example update

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,33 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Brand } from '../src';
+import { Heading, Brand, Post, Card } from "../dist"
 
 const App = () => {
   return (
-    <div>
+    <>
+    <div style={{background: '#000'}}>
       <Brand />
     </div>
+    <Heading>Hello World</Heading>
+    <Post
+      content={{
+        title: 'hello',
+        subtitle: 'world',
+        excerpt: 'It is a test content'
+      }}
+    />
+    <Card
+      content={{
+        header: {
+          title: 'Card test',
+        },
+        footer: {
+          title: 'Card Footer'
+        }
+      }}
+    />
+    </>
   );
 };
 
