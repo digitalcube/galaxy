8174c9ddd8f0f005c325bed1d3cf42d0f6b9e61a
Daniel Olson <emaildano@gmail.com>
2020-08-18 17:46:37 -0400

    updated story for buttons

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -18,46 +18,26 @@ export const buttonData = {
   href: `#`,
 };
 
+const schemas = [`galaxy`, `shifter`, `amimoto`];
+const modes = [`light`, `dark`];
+const kinds = [`primary`, `ghost`, `success`];
+const outline = [true, false];
+
+const options = outline.map(outline => {
+  return kinds.map(kind => {
+    return modes.map(mode => {
+      return schemas.map(schema => {
+        return (
+          <Button outline={outline} mode={mode} schema={schema} kind={kind}>
+            {schema} / {mode} / {kind}
+          </Button>
+        );
+      });
+    });
+  });
+});
+
 export const Buttons = () => {
   const { label, href } = buttonData;
-  return (
-    <>
-      <Button kind="primary" schema="galaxy" href={href}>
-        {label}
-      </Button>
-      <Button kind="primary" schema="amimoto" href={href}>
-        {label}
-      </Button>
-      <Button kind="primary" schema="shifter" href={href}>
-        {label}
-      </Button>
-      <Button kind="success" schema="shifter" href={href}>
-        {label}
-      </Button>
-      <Button kind="primary" outline schema="galaxy" href={href}>
-        {label}
-      </Button>
-      <Button kind="primary" outline schema="amimoto" href={href}>
-        {label}
-      </Button>
-      <Button kind="primary" outline schema="shifter" href={href}>
-        {label}
-      </Button>
-      <Button kind="success" outline schema="shifter" href={href}>
-        {label}
-      </Button>
-      <Button kind="success" outline schema="amimoto" href={href}>
-        {label}
-      </Button>
-      <Button kind="ghost" schema="galaxy" href={href}>
-        {label}
-      </Button>
-      <Button kind="ghost" schema="amimoto" href={href}>
-        {label}
-      </Button>
-      <Button kind="ghost" schema="shifter" href={href}>
-        {label}
-      </Button>
-    </>
-  );
+  return <>{options}</>;
 };
