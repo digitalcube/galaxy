83e14d3cf4b6aa59e5c02175f6921a65dfc0a1a0
Daniel Olson <emaildano@gmail.com>
2020-08-19 11:10:53 -0400

    updated storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
-  stories: ['../src/components/**/*.stories.(js|ts|tsx)'],
+  // stories: ['../src/components/**/*.stories.(js|ts|tsx)'],
+  stories: ['../**/*.stories.@(ts|js|tsx)'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-knobs',
