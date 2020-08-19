d2aefda46dffda7cf1301d9a8b7f6fcd556dd774
Daniel Olson <emaildano@gmail.com>
2020-08-19 11:13:21 -0400

    migrated to ts

--- .storybook/previewTheme.ts
+++ .storybook/previewTheme.ts
@@ -3,7 +3,7 @@ import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { withContexts } from '@storybook/addon-contexts/react';
 import { contexts } from './contexts';
-import { GlobalStyle } from './../src/styles/index';
+import { GlobalStyle } from '../src/styles';
 import { Galaxy } from '../src/components/Galaxy';
 
 addDecorator(withContexts(contexts));
