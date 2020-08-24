74f4be1343574d8bf2aa8a92c907c22bfc9e3dcc
Jason <jmcclintok07@gmail.com>
2020-08-24 11:55:17 -0400

    galaxy primary solid hover styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -65,8 +65,8 @@ const backgroundColor = ({
     if (schema === `galaxy`)
       return theme.variants('mode', 'kind', {
         primary: {
-          light: colors.galaxy.black,
-          dark: colors.galaxy.white,
+          light: colors.galaxy.gray.g700,
+          dark: colors.galaxy.gray.g200,
         },
         success: {
           light: colors.galaxy.successDark,
@@ -221,8 +221,8 @@ const color = ({
     if (schema === `galaxy`)
       return theme.variants('mode', 'kind', {
         primary: {
-          light: colors.galaxy.black,
-          dark: colors.galaxy.white,
+          light: colors.galaxy.white,
+          dark: colors.galaxy.black,
         },
         success: {
           light: colors.galaxy.white,
