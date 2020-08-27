aab6ba065d5edc92e5fe91ad05e3923ad44d27ab
Daniel Olson <emaildano@gmail.com>
2020-08-27 13:54:33 -0400

    updated features

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -37,18 +37,20 @@ export const Features = ({
   });
   return (
     <section>
-      <Container size="md" align="left">
-        <Content>
-          <Heading as="h2" size="lg">
-            {title}
-          </Heading>
-          <Heading as="h2" size="sm">
-            {subtitle}
-          </Heading>
-          <Button outline kind="primary" href={buttonLink}>
-            See all features
-          </Button>
-        </Content>
+      <Container>
+        <Container size="md" align="left">
+          <Content>
+            <Heading as="h2" size="lg">
+              {title}
+            </Heading>
+            <Heading as="h2" size="sm">
+              {subtitle}
+            </Heading>
+            <Button outline kind="primary" href={buttonLink}>
+              See all features
+            </Button>
+          </Content>
+        </Container>
       </Container>
       <Container>
         <Shape shape="square" />
