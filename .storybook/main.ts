0d766b9220f42dbbbe5b69dee32ec47a25145782
Daniel Olson <emaildano@gmail.com>
2020-09-11 13:53:50 -0400

    updated slider width

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,7 @@ module.exports = {
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-knobs',
+    '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-viewport/register',
     '@storybook/addon-contexts/register',
