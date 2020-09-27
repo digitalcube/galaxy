1152973cb402bad13c908d96fa254ca92d41a431
Daniel Olson <emaildano@gmail.com>
2020-09-27 13:11:13 -0400

    removed unused deps

--- src/components/Header/useToggle.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-
-export const useToggle = (initialMode = false) => {
-  const [open, setOpen] = useState(initialMode);
-  const toggle = () => setOpen(!open);
-  return [open, toggle, setOpen] as const;
-};
