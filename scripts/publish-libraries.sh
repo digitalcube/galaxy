2d601fd117e67b7498925fb943535773b3410366
Hidetaka Okamoto <info@wp-kyoto.net>
2021-01-15 15:30:08 +0900

    fix: includes a dependencies packages

--- scripts/publish-libraries.sh
+++ scripts/publish-libraries.sh
@@ -14,20 +14,22 @@ if [ "$AFFECTED" != "" ]; then
   echo "Copy Environment Files"
 
   while IFS= read -r -d $' ' lib; do
+    echo "Building $lib"
+    npm run build "$lib" -- --with-deps 
     echo "Setting version for $lib"
     npm run nx release "$lib"
   done <<<"$AFFECTED " # leave space on end to generate correct output
 
-  # git push origin master
   echo "Push the new tags"
   git push origin master --tags
 
   while IFS= read -r -d $' ' lib; do
     echo "Building $lib"
-    npm run build "$lib" -- --with-deps
+    npm run build "$lib" -- --with-deps --buildableProjectDepsInPackageJsonType=dependencies
     wait
     if [ "$DRY_RUN" == "False" ]; then
       echo "Publishing $lib"
+      #npm publish "$ROOT_DIR/dist/libs/$lib"
       npm publish "$ROOT_DIR/dist/libs/${lib/-//}" # --access=public
     else
       echo "Dry Run, not publishing $lib"
