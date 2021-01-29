161d3be90e1aa4c4951b470f3412078600961609
Daniel Olson <emaildano@gmail.com>
2021-01-29 09:57:09 -0500

    updated border radius

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const colors = require('./libs/core/src/foundation/Color/tailwind.config.js');
 const fontSize = require('./libs/core/src/foundation/FontSize/tailwind.config.js');
 const fontFamily = require('./libs/core/src/foundation/FontFamily/tailwind.config.js');
 const fontWeight = require('./libs/core/src/foundation/FontWeight/tailwind.config.js');
+const borderRadius = require('./libs/core/src/foundation/BorderRadius/tailwind.config.js');
 
 module.exports = {
   theme: {
