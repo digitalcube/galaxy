d1242705ee411fb8f15d8358393d7a8db47b1d3e
Daniel Olson <emaildano@gmail.com>
2021-01-29 20:39:20 -0500

    updated variants

--- libs/core/src/components/Button/galaxy.config.js
+++ libs/core/src/components/Button/galaxy.config.js
@@ -2,13 +2,19 @@ module.exports = {
   schema: {
     components: {
       button: {
-        DEFAULT: 'font-bold py-2 px-4 rounded',
+        DEFAULT:
+          'font-strong py-2 px-4 focus:ring-shifter-magenta-primary rounded',
         variants: {
-          primary: 'bg-shifter-purple-primary hover:bg-shifter-purple-600 text-white',
-          danger: 'bg-status-danger-default text-white',
-          white: 'bg-white text-black',
-          ghost: 'bg-transparent text-black',
-          success: 'bg-status-success-default text-white',
+          primary:
+            'bg-shifter-purple-primary hover:bg-shifter-purple-600 focus:ring focus:bg-shifter-purple-500 text-white disabled:bg-shifter-gray-200 disabled:text-shifter-gray-300',
+          danger:
+            'bg-status-danger-default text-white hover:bg-status-danger-dark',
+          white:
+            'bg-white text-shifter-purple-primary hover:bg-shifter-gray-100 hover:text-shifter-purple-700',
+          ghost:
+            'bg-transparent text-shifter-purple-primary hover:bg-shifter-purple-200 focus:bg-transparent',
+          success:
+            'bg-status-success-default text-white hover:bg-status-success-dark',
         },
       },
     },
