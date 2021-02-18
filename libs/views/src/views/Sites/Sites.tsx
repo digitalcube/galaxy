f389b108b9e50175081927b8e3bf25248092d4e2
Daniel Olson <emaildano@gmail.com>
2021-02-18 16:39:50 -0500

    updated sites

--- libs/views/src/views/Sites/Sites.tsx
+++ libs/views/src/views/Sites/Sites.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { Site, Section } from '@galaxy/core';
+import { SitesSelect } from '@galaxy/views';
 
 export const Sites: FC<Sites> = ({ sites }) => {
   const allSites = sites.map((site) => {
@@ -17,7 +18,7 @@ export const Sites: FC<Sites> = ({ sites }) => {
 
   return (
     <Section className="space-y-10">
-      <Section>Hello world</Section>
+      <SitesSelect />
       <Section className="space-y-4">{allSites}</Section>
     </Section>
   );
--- libs/views/src/views/Sites/index.ts
+++ libs/views/src/views/Sites/index.ts
@@ -1,1 +1,2 @@
 export * from './Sites';
+export * from './SitesSelect';
