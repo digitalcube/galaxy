5fdadfb1d16aa8ee5a1ea709716eae3c3295030b
Daniel Olson <emaildano@gmail.com>
2021-03-03 16:32:10 -0500

    init ThemeProvider for Galaxy core

---
+++ libs/core/src/components/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,52 @@
+// TODO: refactor https://jeffjadulco.com/blog/dark-mode-react-tailwind/#adding-our-css-variables
+
+import React from 'react';
+
+export const ThemeContext = React.createContext();
+
+export const ThemeProvider = ({ initialTheme, children }) => {
+  const [theme, setTheme] = React.useState(getInitialTheme);
+
+  const rawSetTheme = (theme) => {
+    const root = window.document.documentElement;
+    const isDark = theme === 'dark';
+
+    root.classList.remove(isDark ? 'light' : 'dark');
+    root.classList.add(theme);
+
+    localStorage.setItem('color-theme', theme);
+  };
+
+  if (initialTheme) {
+    rawSetTheme(initialTheme);
+  }
+
+  React.useEffect(() => {
+    rawSetTheme(theme);
+  }, [theme]);
+
+  console.log(theme);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+const getInitialTheme = () => {
+  if (typeof window !== 'undefined' && window.localStorage) {
+    const storedPrefs = window.localStorage.getItem('color-theme');
+    if (typeof storedPrefs === 'string') {
+      return storedPrefs;
+    }
+
+    const userMedia = window.matchMedia('(prefers-color-scheme: dark)');
+    if (userMedia.matches) {
+      return 'dark';
+    }
+  }
+
+  // If you want to use light theme as the default, return "light" instead
+  return 'dark';
+};
