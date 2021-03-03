fe76cabe40ece3e2cb36b819b0eefb1d866d42d1
Daniel Olson <emaildano@gmail.com>
2021-03-03 13:50:23 -0500

    updated browser

--- libs/core/src/components/Browser/Browser.tsx
+++ libs/core/src/components/Browser/Browser.tsx
@@ -1,37 +1,28 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import { Image } from '@galaxy/core';
 
-type Props = {
-  className?: string;
-  children?: React.ReactNode;
-};
-
-export const Browser = ({ className, children }: Props) => (
+export const Browser = ({ className, img }: Browser) => (
   <div className={`bg-purple-dark rounded overflow-hidden ${className}`}>
     <svg
-      className="d-block"
+      className="bg-shifter-gray-200"
       xmlns="http://www.w3.org/2000/svg"
-      data-name="Layer 2"
       viewBox="0 0 380 16"
     >
-      <defs>
-        <mask width="380" height="16" x="0" y="0" maskUnits="userSpaceOnUse">
-          <path
-            fill="#fff"
-            fillRule="evenodd"
-            d="M4 0h372a4 4 0 014 4v12H0V4a4 4 0 014-4z"
-          />
-        </mask>
-      </defs>
       <g mask="url(#a)">
-        <circle cx="14" cy="8" r="3" fill="#dad2d7" />
+        <circle cx="14" cy="8" r="3" className="bg-shifter-gray-500" />
       </g>
       <g mask="url(#a)">
-        <circle cx="30" cy="8" r="3" fill="#dad2d7" />
+        <circle cx="30" cy="8" r="3" className="bg-shifter-gray-500" />
       </g>
       <g mask="url(#a)">
-        <circle cx="46" cy="8" r="3" fill="#dad2d7" />
+        <circle cx="46" cy="8" r="3" className="bg-shifter-gray-500" />
       </g>
     </svg>
-    {children}
+    <Image img={img} className="w-full" />
   </div>
 );
+
+export type Browser = {
+  className?: string;
+  img?: string;
+};
