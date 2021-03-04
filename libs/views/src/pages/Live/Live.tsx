cdd740d9dd3c3cc05749040aebf354b23b4f1715
Daniel Olson <emaildano@gmail.com>
2021-03-04 10:01:09 -0500

    resolved error

--- libs/views/src/pages/Live/Live.tsx
+++ libs/views/src/pages/Live/Live.tsx
@@ -2,7 +2,16 @@ import React, { FC } from 'react';
 import { Section, fakerHeader } from '@galaxy/core';
 import { SiteOverview, Site, Dashboard, Aside, Main } from '@galaxy/views';
 
-export const Live: FC<Live> = ({ name }) => {
+export const Live: FC<Live> = ({
+  name,
+  items,
+  artifact,
+  cloudfront,
+  variant,
+  img,
+  date,
+  team,
+}) => {
   return (
     <Dashboard {...fakerHeader}>
       <Section className="md:flex min-h-screen border-t border-shifter-gray-200">
