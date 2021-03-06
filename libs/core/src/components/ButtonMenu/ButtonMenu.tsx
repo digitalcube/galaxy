5cad4bf5d9041ddd79dff82d959bebb87a88dd9a
Daniel Olson <emaildano@gmail.com>
2021-03-06 12:43:53 -0500

    Button Menu scaffolding

--- libs/core/src/components/ButtonMenu/ButtonMenu.tsx
+++ libs/core/src/components/ButtonMenu/ButtonMenu.tsx
@@ -16,11 +16,9 @@ export const ButtonMenu: FC<ButtonMenu> = ({
         <Menu>
           {({ open }) => (
             <>
-              {/* <span className="rounded-md shadow-sm"> */}
               <Menu.Button className="inline-flex rounded-md shadow-sm justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800">
                 {label}
               </Menu.Button>
-              {/* </span> */}
 
               <Transition
                 show={open}
