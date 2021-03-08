b5604bf280ef345cd39bd5b58f3e13c1f7f5f43f
Daniel Olson <emaildano@gmail.com>
2021-03-08 15:23:49 -0500

    checkin

--- libs/core/src/components/Toggle/galaxy.config.ts
+++ libs/core/src/components/Toggle/galaxy.config.ts
@@ -14,17 +14,3 @@ export const toggle = {
     },
   },
 };
-
-// // const toggleButtonCss = `${
-// //   enabled ? `translate-x-${size / 2 + 1}` : 'translate-x-1'
-// // } inline-block w-${size / 2 - 2} h-${
-// //   size / 2 - 2
-// // } transform bg-white rounded-full transition`;
-
-// // const toggleBezelCss = `${
-// //   enabled
-// //     ? 'bg-status-success-default border-status-success-default'
-// //     : 'bg-shifter-gray-300 border-shifter-gray-400'
-// // } relative inline-flex h-${
-// //   size / 2
-// // } rounded-full w-${size} items-center border border-shifter-gray-400`;-
