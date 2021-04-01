aa09eb6a0969cdc31e9290509407f1a55e77c2fa
Hidetaka Okamoto <info@wp-kyoto.net>
2021-04-01 19:34:12 +0900

    page

---
+++ apps/demo/src/app/pages/sites/live/Analytics.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import { Main } from '@galaxy/views';
+
+export const PageSiteAnalytics: FC = () => {
+  return (
+    <Main title="Analytics">
+        <p>Silence is golden.</p>
+    </Main>
+  );
+};
+
+export type PageSiteAnalyticsProps = {
+};
--- apps/demo/src/app/routes/Site.tsx
+++ apps/demo/src/app/routes/Site.tsx
@@ -3,7 +3,6 @@ import { MenuItems } from '@galaxy/core';
 import {
   Redirect,
   Route,
-    RouteComponentProps,
   Switch,
   useParams,
 } from 'react-router-dom';
@@ -22,16 +21,13 @@ import { PageDevEnvironment } from '../pages/sites/dev/Environment';
 import { PageCDN } from '../pages/sites/dev/CDN';
 import { PageWebhooks } from '../pages/sites/dev/Webhooks';
 import { PageSiteSetting } from '../pages/sites/Settings';
+import { PageSiteAnalytics } from '../pages/sites/live/Analytics';
+import { PageSiteAccessLogs } from '../pages/sites/dev/AccessLogs';
+import { PageSiteBasicAuth } from '../pages/sites/dev/BasicAuth';
+import { PageSiteEmergencyAction } from '../pages/sites/dev/EmergencyAction';
+import { PageSiteErrors } from '../pages/sites/dev/Errors';
+import { PageSiteTeams } from '../pages/sites/Teams';
 
-const PageSub: FC<RouteComponentProps> = (props) => {
-    return (
-        <div>
-          <pre>
-            <code>{JSON.stringify(props, null, 2)}</code>
-          </pre>
-        </div>
-    );
-  };
 export const RouteSite: FC = () => {
   const {
     sites,admin,
@@ -111,17 +107,17 @@ export const RouteSite: FC = () => {
               <Switch>
                 <Redirect from={`/${admin}/${sites}/:siteId/home`} to={`/${admin}/${sites}/:siteId/`} />
                 <Route path={`/${admin}/${sites}/:siteId/live/domains`} component={PageDomain} />
-                <Route path={`/${admin}/${sites}/:siteId/live/analytics`} component={PageSub} />
+                <Route path={`/${admin}/${sites}/:siteId/live/analytics`} component={PageSiteAnalytics} />
                 <Route path={`/${admin}/${sites}/:siteId/live`} component={PageLiveOverview} />
                 <Route path={`/${admin}/${sites}/:siteId/staging`} component={PageStaging} />
                 <Route path={`/${admin}/${sites}/:siteId/dev/cdn`} component={PageCDN} />
                 <Route path={`/${admin}/${sites}/:siteId/dev/webhooks`} component={PageWebhooks} />
-                <Route path={`/${admin}/${sites}/:siteId/dev/access-logs`} component={PageSub} />
-                <Route path={`/${admin}/${sites}/:siteId/dev/basic-auth`} component={PageSub} />
-                <Route path={`/${admin}/${sites}/:siteId/dev/emergency-actions`} component={PageSub} />
-                <Route path={`/${admin}/${sites}/:siteId/dev/errors`} component={PageSub} />
+                <Route path={`/${admin}/${sites}/:siteId/dev/access-logs`} component={PageSiteAccessLogs} />
+                <Route path={`/${admin}/${sites}/:siteId/dev/basic-auth`} component={PageSiteBasicAuth} />
+                <Route path={`/${admin}/${sites}/:siteId/dev/emergency-actions`} component={PageSiteEmergencyAction} />
+                <Route path={`/${admin}/${sites}/:siteId/dev/errors`} component={PageSiteErrors} />
                 <Route path={`/${admin}/${sites}/:siteId/dev/`} component={PageDevEnvironment} />
-                <Route path={`/${admin}/${sites}/:siteId/team`} component={PageSub} />
+                <Route path={`/${admin}/${sites}/:siteId/team`} component={PageSiteTeams} />
                 <Route path={`/${admin}/${sites}/:siteId/settings`} component={PageSiteSetting} />
                 <Route path={`/${admin}/${sites}/:siteId/`} exact component={PageOverview} />
               </Switch>
