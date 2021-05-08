73d31efb1f119e6b19520e923e0732be7137857d
Daniel Olson <emaildano@gmail.com>
2021-05-08 15:00:16 -0400

    Added Shifter header to Storybook

--- libs/views/src/components/Dashboard/Dashboard.stories.tsx
+++ libs/views/src/components/Dashboard/Dashboard.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Dashboard } from '@galaxy/views';
 import { defaultStorybookConfig } from '../../.storybook/docs';
+import { fakerAvatar } from '@galaxy/faker';
 export default {
   ...defaultStorybookConfig,
   component: Dashboard,
@@ -9,4 +10,15 @@ export default {
 
 export const Example = () => {
   return <Dashboard />;
+};
+
+export const Shifter = () => {
+  return (
+    <Dashboard
+      header={{
+        logo: './shifter-logomark.svg',
+        avatar: { ...fakerAvatar.avatar },
+      }}
+    />
+  );
 };
