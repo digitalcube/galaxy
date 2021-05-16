2b604cc5e7357614cf2f8567768a0601683be1f5
Daniel Olson <emaildano@gmail.com>
2021-05-16 11:49:26 -0400

    removed unused component

--- libs/shifter-dashboard/src/views/Container/PhpVersion.tsx
+++ libs/shifter-dashboard/src/views/Container/PhpVersion.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
-import { Heading, Section, Php } from '@galaxy/core';
+import { Heading, Section } from '@galaxy/core';
+import { DiPhp } from 'react-icons/di';
 
 export const PhpVersion: FC<PhpVersion> = ({ title =  'PHP version', version }: PhpVersion) => {
   if (!version) return null;
   return (
     <Section className="space-x-2 flex flex-row items-center">
-      <Php />
+      <DiPhp />
       <Heading variant="primary" text={`${title} ${version}`} />
     </Section>
   );
