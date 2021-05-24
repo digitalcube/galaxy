6b5a9e87a7eb291715de5c676e4943f972253a8c
Daniel Olson <emaildano@gmail.com>
2021-05-24 14:57:07 -0400

    Updated domain create

--- libs/shifter-dashboard/src/lib/create-domain/create-domain.tsx
+++ libs/shifter-dashboard/src/lib/create-domain/create-domain.tsx
@@ -1,19 +1,28 @@
-import React, { FC } from 'react';
+import React, { useMemo } from 'react';
 import { Button } from '@galaxy/core';
+import { useInternalLinkBase } from '@galaxy/shifter-dashboard';
 import { HiOutlinePlus } from 'react-icons/hi';
 
-export const CreateDomain: FC<CreateDomain> = ({ title }: CreateDomain) => {
+export interface CreateDomainProps {
+  title?: string;
+  href?: string;
+}
+export function CreateDomain(props: CreateDomainProps) {
+  const { title, href } = props;
+  const { site } = useInternalLinkBase();
+  const target = useMemo(() => {
+    if (href) return href;
+    return `/${site}/create`;
+  }, [href, site]);
   return (
-    <Button variant="primary" className="space-x-3 flex">
+    <Button variant="primary" className="space-x-3 flex" href={target}>
       <HiOutlinePlus />
       <span>{title}</span>
     </Button>
   );
-};
+}
 
-type CreateDomain = {
-  title?: string;
-};
+export default CreateDomain;
 
 CreateDomain.defaultProps = {
   title: 'Add a new domain',
