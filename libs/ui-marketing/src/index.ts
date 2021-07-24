c2ae7bbcd530a764c426e961814a5fd44368a0b8
Daniel Olson <emaildano@gmail.com>
2021-07-24 18:58:29 -0400

    Added marketing hero illo

--- libs/ui-marketing/src/index.ts
+++ libs/ui-marketing/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/hero-simple-illustration/hero-simple-illustration';
 export * from './lib/hero-simple-centered/hero-simple-centered';
 export * from './lib/header-simple/header-simple';
 export * from './lib/ui-marketing';
