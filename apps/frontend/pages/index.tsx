9cdd00b703989132b19fe352508ab27165f1995c
Daniel Olson <emaildano@gmail.com>
2021-08-11 18:49:13 -0400

    Added React map to components

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,3 +1,4 @@
+import { MapToComponents } from 'react-map-to-components';
 import {
   HeroSimpleCentered,
   LogosSimple,
@@ -5,13 +6,27 @@ import {
   CTASimpleLeft,
 } from '@galaxy/ui-marketing';
 
+const list = [
+  { id: 1, type: 'Hero' },
+  { id: 2, type: 'Logos' },
+  { id: 3, type: 'Screenshot' },
+  { id: 4, type: 'CTA' },
+];
+
 export function Index() {
   return (
     <div>
-      <HeroSimpleCentered />
-      <LogosSimple />
-      <LargeScreenshot />
-      <CTASimpleLeft />
+      <MapToComponents
+        getKey={(x) => x.id}
+        getType={(x) => x.type}
+        list={list}
+        map={{
+          Hero: HeroSimpleCentered,
+          Logos: LogosSimple,
+          Screenshot: LargeScreenshot,
+          CTA: CTASimpleLeft,
+        }}
+      />
     </div>
   );
 }
