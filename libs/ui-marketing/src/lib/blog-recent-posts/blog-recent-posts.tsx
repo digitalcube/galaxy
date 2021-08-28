6e0fd966585c5101a21d92dfa4644b8496ba0103
Daniel Olson <emaildano@gmail.com>
2021-08-28 19:42:55 -0400

    updated starting blog

---
+++ libs/ui-marketing/src/lib/blog-recent-posts/blog-recent-posts.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface BlogRecentPostsProps {}
+
+export function BlogRecentPosts(props: BlogRecentPostsProps) {
+  return (
+    <div>
+      <h1>Welcome to blog-recent-posts!</h1>
+    </div>
+  );
+}
+
+export default BlogRecentPosts;
---
+++ libs/ui-marketing/src/lib/blog/blog.tsx
@@ -0,0 +1,33 @@
+import { MapToComponents } from 'react-map-to-components';
+import { BlogRecentPosts } from '@galaxy/ui-marketing';
+
+/* eslint-disable-next-line */
+export interface BlogProps {
+  pattern?: string;
+}
+
+const list = [
+  { id: 1, type: 'BlogRecentPosts' },
+];
+
+export function Blog(props: BlogProps) {
+  const { pattern } = props;
+  const patternId = [list.find((i) => i.type === pattern)];
+  return (
+    <div>
+      <MapToComponents
+        getKey={(x) => x.id}
+        getType={(x) => x.type}
+        list={patternId}
+        map={{
+          BlogRecentPosts,
+        }}
+        mapDataToProps={{
+          BlogRecentPosts: ({ data }) => ({ ...data }),
+        }}
+      />
+    </div>
+  );
+}
+
+export default Blog;
