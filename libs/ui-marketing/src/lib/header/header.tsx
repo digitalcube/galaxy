feebac9425c5ba95dac82e2e278ca2a4de37652a
Daniel Olson <emaildano@gmail.com>
2021-10-13 12:40:09 -0400

    Updated header, WIP

--- libs/ui-marketing/src/lib/header/header.tsx
+++ libs/ui-marketing/src/lib/header/header.tsx
@@ -3,18 +3,16 @@ import { HeaderSimple } from '../header-simple/header-simple';
 /* eslint-disable-next-line */
 export interface HeaderProps {}
 
+export function Header(props: HeaderProps) {
+  return <HeaderSimple {...props} />;
+}
+
+export default Header;
+
 const header = {
   brand: {
     logo: './labworks-logo-primary.svg',
   },
   primary: [],
-  secondary: [
-    { children: 'Work with us', href: '#', variant: 'primary' },
-  ]
+  secondary: [{ children: 'Work with us', href: '#', variant: 'primary' }],
 };
-
-export function Header(props: HeaderProps) {
-  return <HeaderSimple {...header} />;
-}
-
-export default Header;
