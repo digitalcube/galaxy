418ea13a582c9c80a0650aa08843121abec0f980
Jason <jmcclintok07@gmail.com>
2021-11-02 16:06:33 -0400

    added test component

--- libs/ui-marketing/src/index.ts
+++ libs/ui-marketing/src/index.ts
@@ -17,3 +17,4 @@ export * from './lib/hero-simple-illustration/hero-simple-illustration';
 export * from './lib/hero-simple-centered/hero-simple-centered';
 export * from './lib/header-simple/header-simple';
 export * from './lib/ui-marketing';
+export * from './lib/test-component/test-component';
---
+++ libs/ui-marketing/src/lib/test-component/test-component.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable-next-line */
+export interface TestComponentProps {}
+
+export function TestComponent(props: TestComponentProps) {
+  return (
+    <div className="bg-black text-white">
+      <p>
+        Open source
+        </p>
+      <h1 className="text-bold">What we're working on</h1>
+    </div>
+  );
+}
+
+export default TestComponent;
