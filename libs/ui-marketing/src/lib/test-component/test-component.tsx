d15c020e028b2d2d39ff9f1c38946418f955935a
Jason <jmcclintok07@gmail.com>
2021-11-04 15:27:11 -0400

    updated responsive styles and added links to test component

--- libs/ui-marketing/src/lib/test-component/test-component.tsx
+++ libs/ui-marketing/src/lib/test-component/test-component.tsx
@@ -5,68 +5,68 @@ export function TestComponent(props: TestComponentProps) {
   return (
     <div className="grid grid-cols-12 h-screen bg-black place-content-center text-white py-40">
       <div className="col-start-3 col-span-8 mb-8">
-        <p className="text-5xl">Open Soure</p>
-        <p className="text-[2.125rem] font-bold">What we're working on</p>
+          <p className="text-5xl">Open Soure</p>
+          <p className="text-[2.125rem] font-bold">What we're working on</p>
       </div>
-      <div className="md col-span-8 lg:col-span-4 col-start-3">
-        <div className="mb-8">
+      <div className="lg:col-span-4 sm:col-span-8 lg:col-start-3 sm:col-start-3 hover:underline mb-8">
+        <a href="https://amimoto-ami.com/">
           <p className="text-[1.125rem] font-bold">Amimoto AMI</p>
           <p>
             Our one-click AMIs are the easiest and easiest way to launch
             WordPress in the cloud.
           </p>
-        </div>
+        </a>
       </div>
-      <div className="md col-span-8 lg:col-span-4 md:col-start-3 lg:col-start-8">
-        <div className="mb-8">
+      <div className="lg:col-span-4 sm:col-span-8 lg:col-start-8 sm:col-start-3 mb-8 hover:underline">
+        <a href="https://github.com/digitalcube/shifter-static-local">
           <p className="text-[1.125rem] font-bold">Shifter Local</p>
           <p>
             Docker image for testing WordPress themes and plugins while
             migrating to Shifter.
           </p>
-        </div>
+        </a>
       </div>
-      <div className="md col-span-8 lg:col-span-4 col-start-3">
-        <div className="mb-8">
+      <div className="lg:col-span-4 sm:col-span-8 lg:col-start-3 sm:col-start-3 mb-8 hover:underline">
+        <a href="https://github.com/getshifter/wp-serverless-forms">
           <p className="text-[1.125rem] font-bold">WP Serverless Forms</p>
           <p>
             Replace PHP contact form actions with HTTP endpoints such IFTTT,
             Basin and more.
           </p>
-        </div>
+        </a>
       </div>
-      <div className="md col-span-8 lg:col-span-4 md:col-start-3 lg:col-start-8">
-        <div className="mb-8">
+      <div className="lg:col-span-4 sm:col-span-8 lg:col-start-8 sm:col-start-3 hover:underline mb-8">
+        <a href="https://github.com/getshifter/wp-serverless-search">
           <p className="text-[1.125rem] font-bold">WP Serverless Search</p>
           <p>
             Search thousands of posts in milliseconds, with fuzzy matching and
             support for custom themes.
           </p>
-        </div>
+        </a>
       </div>
-      <div className="md col-span-8 lg:col-span-4 col-start-3">
-        <div className="mb-8">
+      <div className="lg:col-span-4 sm:col-span-8 lg:col-start-3 sm:col-start-3 mb-8 hover:underline">
+        <a href="https://amimoto-ami.com/products/aws-cloudformation-wordpress/">
           <p className="text-[1.125rem] font-bold">Jinkei</p>
           <p>
             Simultaneously launch EC2, RDS, Load Balancers and more for
             WordPress using Amimoto AMI.
           </p>
-        </div>
+        </a>
       </div>
-      <div className="md col-span-8 lg:col-span-4 md:col-start-3 lg:col-start-8">
-        <div className="mb-8">
+      <div className="lg:col-span-4 sm:col-span-8 lg:col-start-8 sm:col-start-3 hover:underline mb-8">
+        <a href="https://www.figma.com/community/file/956241516043940295/Shifter-Components-for-Galaxy">
           <p className="text-[1.125rem] font-bold">Galaxy Design Components</p>
           <p>
             Figma library containing a wide range of components for the Galaxy
             design system.
           </p>
-        </div>
+        </a>
       </div>
-      <div className="md col-span-8 lg:col-span-4 col-start-3">
-        <div className="mb-8">
+      <div className="lg:col-span-4 sm:col-span-8 lg:col-start-3 sm:col-start-3 mb-8 hover:underline">
+        <a href="https://www.npmjs.com/package/@shifter/cli">
           <p className="text-[1.125rem] font-bold">Shifter CLI</p>
           <p>Use WordPress as a headless CMS</p>
-        </div>
+        </a>
       </div>
     </div>
   );
