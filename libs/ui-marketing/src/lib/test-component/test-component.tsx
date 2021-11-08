882a01b6c9243198b49cc4ef1e955534d786db53
Jason <jmcclintok07@gmail.com>
2021-11-08 16:43:52 -0500

    reverted links to a tags and added icons

--- libs/ui-marketing/src/lib/test-component/test-component.tsx
+++ libs/ui-marketing/src/lib/test-component/test-component.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 
 /* eslint-disable-next-line */
 export interface TestComponentProps {}
@@ -11,64 +11,87 @@ export function TestComponent(props: TestComponentProps) {
         <p className="text-[2.125rem] font-bold">What we're working on</p>
       </div>
       <div className="lg:col-span-4 md:col-span-8 lg:col-start-3 md:col-start-3 hover:underline mb-8">
-        <Link href="https://amimoto-ami.com/">
-          <p className="text-[1.125rem] font-bold">Amimoto AMI</p>
+        <a href="https://amimoto-ami.com/">
+          <div className="text-[1.125rem] font-bold flex">
+            <div className="flex-initial">Amimoto AMI</div>
+            <FaExternalLinkAlt className="ml-2 pt-1.5 flex-initial" />
+          </div>
           <p>
             Our one-click AMIs are the easiest and easiest way to launch
             WordPress in the cloud.
           </p>
-        </Link>
+        </a>
       </div>
       <div className="lg:col-span-4 md:col-span-8 lg:col-start-8 md:col-start-3 mb-8 hover:underline">
-        <Link href="https://github.com/digitalcube/shifter-static-local">
-          <p className="text-[1.125rem] font-bold">Shifter Local</p>
+        <a href="https://github.com/digitalcube/shifter-static-local">
+          <div className="text-[1.125rem] font-bold flex">
+            <div className-="flex-initial">
+            Shifter Local 
+          </div>
+          <FaExternalLinkAlt className="ml-2 pt-1.5 flex-initial"/>
+          </div>
           <p>
             Docker image for testing WordPress themes and plugins while
             migrating to Shifter.
           </p>
-        </Link>
+        </a>
       </div>
       <div className="lg:col-span-4 md:col-span-8 lg:col-start-3 md:col-start-3 mb-8 hover:underline">
-        <Link href="https://github.com/getshifter/wp-serverless-forms">
-          <p className="text-[1.125rem] font-bold">WP Serverless Forms</p>
+        <a href="https://github.com/getshifter/wp-serverless-forms">
+          <div className="text-[1.125rem] font-bold flex">
+            <div className="flex-initial">WP Serverless Forms</div>
+            <FaExternalLinkAlt className="ml-2 pt-1.5 flex-initial" />
+          </div>
           <p>
             Replace PHP contact form actions with HTTP endpoints such IFTTT,
             Basin and more.
           </p>
-        </Link>
+        </a>
       </div>
       <div className="lg:col-span-4 md:col-span-8 lg:col-start-8 md:col-start-3 hover:underline mb-8">
-        <Link href="https://github.com/getshifter/wp-serverless-search">
-          <p className="text-[1.125rem] font-bold">WP Serverless Search</p>
+        <a href="https://github.com/getshifter/wp-serverless-search">
+          <div className="text-[1.125rem] font-bold flex">
+            <div className="flex-initial">WP Serverless Search</div>
+            <FaExternalLinkAlt className="ml-2 pt-1.5 flex-initial" />
+          </div>
           <p>
             Search thousands of posts in milliseconds, with fuzzy matching and
             support for custom themes.
           </p>
-        </Link>
+        </a>
       </div>
       <div className="lg:col-span-4 md:col-span-8 lg:col-start-3 md:col-start-3 mb-8 hover:underline">
-        <Link href="https://amimoto-ami.com/products/aws-cloudformation-wordpress/">
-          <p className="text-[1.125rem] font-bold">Jinkei</p>
+        <a href="https://amimoto-ami.com/products/aws-cloudformation-wordpress/">
+          <div className="text-[1.125rem] font-bold flex">
+            <div className="flex-initial">Jinkei</div>
+            <FaExternalLinkAlt className="ml-2 pt-1.5 flex-initial" />
+          </div>
           <p>
             Simultaneously launch EC2, RDS, Load Balancers and more for
             WordPress using Amimoto AMI.
           </p>
-        </Link>
+        </a>
       </div>
       <div className="lg:col-span-4 md:col-span-8 lg:col-start-8 md:col-start-3 hover:underline mb-8">
-        <Link href="https://www.figma.com/community/file/956241516043940295/Shifter-Components-for-Galaxy">
-          <p className="text-[1.125rem] font-bold">Galaxy Design Components</p>
+        <a href="https://www.figma.com/community/file/956241516043940295/Shifter-Components-for-Galaxy">
+          <div className="text-[1.125rem] font-bold flex">
+            <div className="flex-initial">Galaxy Design Components</div>
+            <FaExternalLinkAlt className="ml-2 pt-1.5 flex-initial" />
+          </div>
           <p>
             Figma library containing a wide range of components for the Galaxy
             design system.
           </p>
-        </Link>
+        </a>
       </div>
       <div className="lg:col-span-4 md:col-span-8 lg:col-start-3 md:col-start-3 mb-8 hover:underline">
-        <Link href="https://www.npmjs.com/package/@shifter/cli">
-          <p className="text-[1.125rem] font-bold">Shifter CLI</p>
+        <a href="https://www.npmjs.com/package/@shifter/cli">
+          <div className="text-[1.125rem] font-bold flex">
+            <div className="flex-initial">Shifter CLI</div>
+            <FaExternalLinkAlt className="ml-2 pt-1.5 flex-initial" />
+          </div>
           <p>Use WordPress as a headless CMS</p>
-        </Link>
+        </a>
       </div>
     </div>
   );
