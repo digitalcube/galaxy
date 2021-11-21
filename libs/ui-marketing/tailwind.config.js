58356e5e232716e3aeccdd749660b3ca3bbff091
Daniel Olson <emaildano@gmail.com>
2021-11-21 12:04:39 -0500

    Updated Storybook and Tailwind

---
+++ libs/ui-marketing/tailwind.config.js
@@ -0,0 +1,19 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+const { join } = require('path');
+
+module.exports = {
+  mode: 'jit',
+  presets: [require('../../tailwind-workspace-preset')],
+  purge: [
+    join(__dirname, 'src/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
