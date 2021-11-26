558cce832f85d5d1ad035bcf205a7a0800006c2d
Daniel Olson <emaildano@gmail.com>
2021-11-26 12:46:11 -0500

    Updated content

--- apps/frontend/pages/content.ts
+++ apps/frontend/pages/content.ts
@@ -9,131 +9,18 @@ export const data = [
   },
   {
     _uid: '3e02ade8',
-    component: 'LargeScreenshot',
+    component: 'ListGridWithIcons',
   },
   {
-    _uid: 'eb010551',
-    component: 'CTASimpleCentered',
+    _uid: 'te32af9a8',
+    component: 'CTAAboutUs',
   },
   {
-    _uid: 'b5515d01',
-    component: 'BenefitsWithIllustration',
+    _uid: 'y83678sj',
+    component: 'LogosSimple',
+  },
+  {
+    _uid: 'bee2af9a8',
+    component: 'ListGrid',
   },
 ];
-
-// export const homePageContent = [
-//   { id: 1, type: 'Hero', pattern: 'HeroSimpleIllustration' },
-//   { id: 2, type: 'CTA', pattern: 'CTAAboutUs' },
-//   { id: 3, type: 'Screenshot', screenshot: '/shifter-dashboard.png' },
-//   { id: 4, type: 'CTA', pattern: 'CTASimpleLeft' },
-//   {
-//     id: 5,
-//     type: 'Benefits',
-//     headline: 'Static WordPress - Blazing Fast',
-//     alternativeHeadline: 'Virtually unhackable WordPress sites',
-//     image: {
-//       url: '/benefit-start-wordpress.png',
-//     },
-//     description:
-//       'Shifter automatically handles your WordPress install and creates scalable sites with no downtime.',
-//     benefits: [
-//       {
-//         title: 'For Businesses',
-//         description:
-//           'Save on maintenance costs while ensuring that your site is always there when your customers need access.',
-//       },
-//       {
-//         title: 'For Designers',
-//         description:
-//           'Focus on design without worrying about the back end. Shiftergives you the freedom to focus on your design while our system takes care of the rest.',
-//       },
-//       {
-//         title: 'For Developers',
-//         description:
-//           'Never worry about wasting time mantaining a WordPress install. Shifter’s containerized environemnet ensures near unimlited scalability with no down time.',
-//       },
-//     ],
-//     action: {
-//       title: 'Start your free trial',
-//     },
-//   },
-//   {
-//     id: 6,
-//     type: 'Testimonial',
-//     reviewBody:
-//       '“I’ve been using this stack for 6 months now. Initially there was a little learning curve with the setup but now I’m flying on the command line and my website are fast! Yes you pay for software, but it’s worth it and you can get more performance out of a lower spec.”',
-//   },
-//   {
-//     id: 7,
-//     type: 'Benefits',
-//     align: 'right',
-//     headline: 'Static WordPress - Blazing Fast',
-//     alternativeHeadline: 'Virtually unhackable WordPress sites',
-//     image: {
-//       url: '/benefit-security-built-in.png',
-//     },
-//     description:
-//       'Shifter automatically handles your WordPress install and creates scalable sites with no downtime.',
-//     benefits: [
-//       {
-//         title: 'For Businesses',
-//         description:
-//           'Save on maintenance costs while ensuring that your site is always there when your customers need access.',
-//       },
-//       {
-//         title: 'For Designers',
-//         description:
-//           'Focus on design without worrying about the back end. Shifter gives you the freedom to focus on your design while our system takes care of the rest.',
-//       },
-//       {
-//         title: 'For Developers',
-//         description:
-//           'Never worry about wasting time maintaining a WordPress install. Shifter’s containerized environment ensures near unlimited scalability with no down time.',
-//       },
-//     ],
-//   },
-//   {
-//     id: 8,
-//     type: 'Testimonial',
-//     reviewBody:
-//       '“I’ve been using this stack for 6 months now. Initially there was a little learning curve with the setup but now I’m flying on the command line and my website are fast! Yes you pay for software, but it’s worth it and you can get more performance out of a lower spec.”',
-//   },
-//   {
-//     id: 9,
-//     type: 'Benefits',
-//     headline: 'Never worry about updates again',
-//     alternativeHeadline: 'Maintenance Free',
-//     image: {
-//       url: '/benefit-security-built-in.png',
-//     },
-//     description:
-//       'Shifter automatically handles your WordPress install and creates sites that are scalable with no downtime.',
-//     benefits: [
-//       {
-//         title: 'For Businesses',
-//         description:
-//           'Save on maintenance costs while ensuring that your site is always there when your customers need access.',
-//       },
-//       {
-//         title: 'For Designers',
-//         description:
-//           'Focus on design without worrying about the back end. Shiftergives you the freedom to focus on your design while our system takes care of the rest.',
-//       },
-//       {
-//         title: 'For Developers',
-//         description:
-//           'Never worry about wasting time mantaining a WordPress install. Shifter’s containerized environemnet ensures near unimlited scalability with no down time.',
-//       },
-//     ],
-//   },
-//   {
-//     id: 10,
-//     type: 'Testimonial',
-//     reviewBody:
-//       '“I’ve been using this stack for 6 months now. Initially there was a little learning curve with the setup but now I’m flying on the command line and my website are fast! Yes you pay for software, but it’s worth it and you can get more performance out of a lower spec.”',
-//   },
-//   { id: 11, type: 'CTA', pattern: 'CTAGetStarted' },
-//   { id: 12, type: 'Blog', pattern: 'BlogRecentPosts' },
-//   { id: 13, type: 'Footer', pattern: 'FooterColNewsletter' },
-//   { id: 14, type: 'List', pattern: 'ListGrid' },
-// ];
