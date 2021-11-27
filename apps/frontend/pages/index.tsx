2699fedd28b8788cde8bbeb28124d576aebefb7c
Daniel Olson <emaildano@gmail.com>
2021-11-27 14:33:33 -0500

    Updated map

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -2,7 +2,7 @@ import { LabWorks as content } from '@galaxy/content';
 import { MapComponents } from '@galaxy/utils';
 
 export function Index(props) {
-  return props.data.map((block) => MapComponents(block));;
+  return props.data.map((block) => MapComponents(block));
 }
 
 export default Index;
