ef6ae5f373b1724a55ec842a89318ac15f4f8b29
Daniel Olson <emaildano@gmail.com>
2021-11-26 19:02:54 -0500

    Merge

---
+++ libs/content/src/lib/lab-works.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable-next-line */
+export interface LabWorksProps {
+  content: Record<string, unknown>;
+}
+
+export const LabWorks = [
+  {
+    _uid: 'H8367kAk',
+    component: 'HeaderSimple',
+  },
+  {
+    _uid: 'BUY6Drn9e1',
+    component: 'HeroSimpleIllustration',
+  },
+  {
+    _uid: 'gJZoSLkfZV',
+    component: 'CTAAboutUs',
+  },
+  {
+    _uid: '3e02ade8',
+    component: 'ListGridWithIcons',
+  },
+  {
+    _uid: 'te32af9a8',
+    component: 'CTAAboutUs',
+  },
+  {
+    _uid: 'y83678sj',
+    component: 'LogosSimple',
+  },
+  {
+    _uid: 'bee2af9a8',
+    component: 'ListGrid',
+  },
+  {
+    _uid: 'nhe8638zfr',
+    component: 'ContactFormSimple',
+  },
+];
