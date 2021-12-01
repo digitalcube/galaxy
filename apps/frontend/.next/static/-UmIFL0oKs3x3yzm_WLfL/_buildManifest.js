afb4cb36459b207cc7ca525c51335be7495c2fc6
Daniel Olson <emaildano@gmail.com>
2021-12-01 17:32:15 -0500

    Updated deps

---
+++ apps/frontend/.next/static/-UmIFL0oKs3x3yzm_WLfL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ae5df09748eb1e0e.js"],"/_error":["static/chunks/pages/_error-d20da8f9a50c916f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
