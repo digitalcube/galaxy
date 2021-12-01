465c99493a83257b406435583505adfb61ec6cda
Daniel Olson <emaildano@gmail.com>
2021-12-01 17:37:59 -0500

    removed untracked files

--- apps/frontend/.next/static/-UmIFL0oKs3x3yzm_WLfL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ae5df09748eb1e0e.js"],"/_error":["static/chunks/pages/_error-d20da8f9a50c916f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
