446eb50092c0382d338088e30aa7be0ee06eab0e
Daniel Olson <emaildano@gmail.com>
2021-12-22 18:55:55 -0500

    Added Swiper

--- libs/ui-marketing/src/lib/cta-about-us/cta-about-us.tsx
+++ libs/ui-marketing/src/lib/cta-about-us/cta-about-us.tsx
@@ -1,17 +1,42 @@
 import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);
 import { BsCircle, BsCircleFill } from 'react-icons/bs';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import Swiper core and required modules
+import SwiperCore, { EffectFade } from 'swiper';
+
+SwiperCore.use([EffectFade ]);
+
+gsap.registerPlugin(ScrollTrigger);
 
 /* eslint-disable-next-line */
 export interface CTAAboutUsProps {}
 
 const features = [
   {
-    name: 'What we do',
+    name: 'We Study Emerging Technologies',
     description:
-      'We work with both existing DigitalCube customers and clients looking to improve their online presence.',
+      'We do research and analyze emerging and under researched technologies and the implications for our clients.',
+  },
+
+  {
+    name: 'We Offer Technology Services',
+    description:
+      'We are a team of WordPress experts and AWS Certified Engineers with experience collaborating with designers, entrepreneurs, enterprises, and startups.',
+  },
+  {
+    name: 'We Offer Design Consulting',
+    description:
+      'Our Design Strategy service helps clients understand how the people they serve will use their products and services.',
+  },
+  {
+    name: 'We Teach Emerging Technologies',
+    description:
+      'In the past we have offered that sponsored workshops about ReactJS and HTML5 to aspiring web developers. These workshops that serve as a bridge from reading tutorials online to mastering the fundamentals of these powerful development techniques.',
   },
 ];
 
@@ -82,27 +107,35 @@ export function CTAAboutUs(props: CTAAboutUsProps) {
           <h2 className="text-size-8 font-bold">Our Process</h2>
         </div>
         <div className="lg:col-start-8 lg:col-span-4 md:col-start-3 md:col-span-8 md:pt-6">
+          <img
+            alt=""
+            src="spotIllustration-eye.svg"
+            className="mb-8"
+            width="71"
+            height="93"
+          />
           <div className="flex space-x-3">
             <BsCircleFill size="12" />
             <BsCircle size="12" />
             <BsCircle size="12" />
           </div>
-          <dl className="mt-6 lg:gap-8">
+          <Swiper
+            tag="dl"
+            className="mt-6 lg:gap-8"
+            slidesPerView={1}
+            effect="fade"
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
             {features.map((feature) => (
-              <div key={feature.name}>
-                <img
-                  alt=""
-                  src="spotIllustration-eye.svg"
-                  width="71"
-                  height="93"
-                />
+              <SwiperSlide key={feature.name}>
                 <dt>
                   <p className="mt-4 text-size-7 font-bold">{feature.name}</p>
                 </dt>
                 <dd className="mt-6 text-size-7">{feature.description}</dd>
-              </div>
+              </SwiperSlide>
             ))}
-          </dl>
+          </Swiper>
         </div>
       </div>
     </section>
